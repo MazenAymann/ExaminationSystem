544fbcfc5fa373423b9a3d65933d437928f2922a
MazenAymann <mazenaymanmo22@gmail.com>
2024-03-01 00:02:34 +0200

    Handling Creating Exam
    Department and Course are now required

--- Project/Models/ExamStructureBLL.cs
+++ Project/Models/ExamStructureBLL.cs
@@ -3,19 +3,19 @@ using WebApplication1.Context;
 
 namespace WebApplication1.Models
 {
-	public static class ExamStructureBLL
-	{
-		private static readonly ExaminationSystemContext _context = new();
+    public static class ExamStructureBLL
+    {
+        private static readonly ExaminationSystemContext _context = new();
 
-		public static int createExam(int courseId, int departmentId, int numMcq, int numTf, int duration, DateTime date, TimeOnly time)
-		{
-			string formattedDate = date.ToString("yyyy-MM-dd");
-			string formattedTime = time.ToString("HH:mm:ss");
+        public static int createExam(int courseId, int departmentId, int numMcq, int numTf, int duration, DateTime date, TimeOnly time)
+        {
+            string formattedDate = date.ToString("yyyy-MM-dd");
+            string formattedTime = time.ToString("HH:mm:ss");
 
-			Console.WriteLine(departmentId);
+            Console.WriteLine(departmentId);
 
-			return _context.Database.ExecuteSqlRaw($"EXEC Create_Exams_For_Department {departmentId}, {courseId}, {duration}, {numMcq}, {numTf}, '{formattedDate}', '{formattedTime}'");
-		}
+            return _context.Database.ExecuteSqlRaw($"EXEC Create_Exams_For_Department {departmentId}, {courseId}, {duration}, {numMcq}, {numTf}, '{formattedDate}', '{formattedTime}'");
+        }
 
-	}
+    }
 }
